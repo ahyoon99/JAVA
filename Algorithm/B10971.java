d7f8759761318468316308237a021104de8938ec
ahyoon99 <kimay9623@naver.com>
2022-02-24 17:03:22 +0900

    10971 외판원 순회 2

---
+++ Algorithm/B10971.java
@@ -0,0 +1,77 @@
+package Algorithm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B10971 {
+
+    public static boolean next_permutation(int[] arr, int n){
+        
+        int i=n-1;
+        while(i>0 && arr[i-1]>=arr[i]){
+            i--;
+        }
+        if(i==0){
+            return false;
+        }
+
+        int j=n-1;
+        while(j>0 && arr[i-1]>=arr[j]){
+            j--;
+        }
+
+        int temp = arr[i-1];
+        arr[i-1]=arr[j];
+        arr[j]=temp;
+
+        Arrays.sort(arr, i,n);
+        
+        return true;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] price = new int[n][n];
+        int[] city = new int[n];
+
+        for(int i=0;i<n;i++){
+            city[i]=i;
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                price[i][j]=sc.nextInt();
+            }
+        }
+
+        int min = 100000000;
+        do{
+            int result=0;
+            boolean check=true;
+            boolean exist = true;
+            int i=0;
+            while(check){
+                if(i>=n-1){
+                    check=false;
+                    break;
+                }
+                if(price[city[i]][city[i+1]]==0){
+                    exist = false;
+                    break;
+                }
+                else{
+                    result+=price[city[i]][city[i+1]];
+                }
+                i++;
+            }
+            result+=price[city[n-1]][city[0]];
+            if(exist==true && price[city[n-1]][city[0]]!=0 && min>result){
+                min=result;
+            }
+        }while(next_permutation(city, n));
+
+        System.out.println(min);
+
+    }
+    
+}
