677d91ba5fc5cf876c6d6c3b5947c084b52d33c6
ahyoon99 <kimay9623@naver.com>
2022-03-08 18:13:56 +0900

    6603 로또 (recursion)

--- Algorithm/B6603.java
+++ Algorithm/B6603.java
@@ -5,54 +5,91 @@ import java.util.Scanner;
 
 public class B6603 {
 
-    public static boolean next_permutation(int[] arr, int n){
-        int i=n-1;
-        while(i>0 && arr[i-1]>=arr[i]){
-            i--;
+    // 1. 순열 사용
+    // public static boolean next_permutation(int[] arr, int n){
+    //     int i=n-1;
+    //     while(i>0 && arr[i-1]>=arr[i]){
+    //         i--;
+    //     }
+    //     if(i==0){
+    //         return false;
+    //     }
+    //     int j=n-1;
+    //     while(j>0 && arr[i-1]>=arr[j]){
+    //         j--;
+    //     }
+    //     int temp=arr[i-1];
+    //     arr[i-1] = arr[j];
+    //     arr[j]=temp;
+    //     Arrays.sort(arr, i,n);
+    //     return true;
+    // }
+    // public static void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+        
+    //     while(true){
+    //         int n=sc.nextInt();
+    //         if(n==0){
+    //             return;
+    //         }
+    //         int[] num = new int[n];
+    //         int[] arr = new int[n];
+    //         for(int i=0;i<n;i++){
+    //             num[i] = sc.nextInt();
+    //         }
+    //         Arrays.sort(num);
+
+    //         for(int i=n-1;i>=6;i--){
+    //             arr[i]=1;
+    //         }
+
+    //         do{
+    //             for(int i=0;i<n;i++){
+    //                 if(arr[i]==0){
+    //                     System.out.print(num[i]+" ");
+    //                 }
+    //             }
+    //             System.out.println();
+    //         }while(next_permutation(arr, n));
+    //         System.out.println();
+
+    //     }
+    // }
+    
+    // 2. 재귀함수 사용
+
+    public static void go(int n, int[] arr, int[] lotto, int i, int size){
+        if(size==n){
+            for(int index=0;index<n;index++){
+                System.out.print(lotto[index]+" ");
+            }
+            System.out.println();
+            return;
         }
-        if(i==0){
-            return false;
+        if(i>=arr.length){
+            return;
         }
-        int j=n-1;
-        while(j>0 && arr[i-1]>=arr[j]){
-            j--;
-        }
-        int temp=arr[i-1];
-        arr[i-1] = arr[j];
-        arr[j]=temp;
-        Arrays.sort(arr, i,n);
-        return true;
+
+        lotto[size]=arr[i];
+        go(n,arr,lotto,i+1,size+1);
+        lotto[size]=0;
+        go(n,arr,lotto,i+1,size);
+        
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        
         while(true){
-            int n=sc.nextInt();
-            if(n==0){
+            int num = sc.nextInt();
+            if(num==0){
                 return;
             }
-            int[] num = new int[n];
-            int[] arr = new int[n];
-            for(int i=0;i<n;i++){
-                num[i] = sc.nextInt();
+            int[] arr = new int[num];
+            int[] lotto = new int[6];
+            for(int i=0;i<num;i++){
+                arr[i] = sc.nextInt();
             }
-            Arrays.sort(num);
-
-            for(int i=n-1;i>=6;i--){
-                arr[i]=1;
-            }
-
-            do{
-                for(int i=0;i<n;i++){
-                    if(arr[i]==0){
-                        System.out.print(num[i]+" ");
-                    }
-                }
-                System.out.println();
-            }while(next_permutation(arr, n));
+            go(6,arr,lotto,0,0);
             System.out.println();
-
         }
     }
-    
 }
