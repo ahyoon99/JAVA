30a009a9e38d06e17dc7a7b825d927d1d29220b3
ahyoon99 <kimay9623@naver.com>
2022-04-02 16:42:43 +0900

    15649 N과 M (1)

--- Algorithm/B15649.java
+++ Algorithm/B15649.java
@@ -2,35 +2,73 @@ package Algorithm;
 import java.util.Scanner;
 
 public class B15649 {
-    public static void go(boolean[] check, int[] ans, int index, int n, int m){
-        if(index == m){
+
+    // 방법 1
+    // public static void go(boolean[] check, int[] ans, int index, int n, int m){
+    //     if(index == m){
+    //         for(int i=0;i<m;i++){
+    //             System.out.print(ans[i]+" ");
+    //         }
+    //         System.out.println();
+    //         return;
+    //     }
+
+    //     for(int i=1;i<=n;i++){
+    //         if(check[i]==true){
+    //             continue;
+    //         }
+    //         else{
+    //             ans[index]=i;
+    //             check[i]=true;
+    //             go(check, ans, index+1, n,m);
+    //             check[i]=false;
+    //         }
+    //     }
+
+    // }
+    // public static void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+    //     int m = sc.nextInt();
+    //     boolean[] check = new boolean[n+1];
+
+    //     int[] arr = new int[n];
+    //     go(check, arr, 0,n,m);
+    // }   
+    
+    // 방법 2 : Code Plus 강의 내용
+    static boolean[] check = new boolean[9];
+    static int[] arr = new int[9];
+
+    public static void go(int index, int n, int m){
+        if(index==m){
             for(int i=0;i<m;i++){
-                System.out.print(ans[i]+" ");
+                System.out.print(arr[i]+" ");
             }
             System.out.println();
+        }
+        if(index>=m){
             return;
         }
-
         for(int i=1;i<=n;i++){
-            if(check[i]==true){
+            if(check[i]){
                 continue;
             }
             else{
-                ans[index]=i;
                 check[i]=true;
-                go(check, ans, index+1, n,m);
+                arr[index]=i;
+                go(index+1, n,m);
                 check[i]=false;
+                arr[index]=0;
             }
         }
-
     }
-    public static void main(String args[]){
+
+    public static void main(String  args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        boolean[] check = new boolean[n+1];
 
-        int[] arr = new int[n];
-        go(check, arr, 0,n,m);
-    }                     
+        go(0,n,m);
+    }
 }
