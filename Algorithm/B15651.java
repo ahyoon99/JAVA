1ee9a217749f1cf6c6499c9a36d5913777621c6e
ahyoon99 <kimay9623@naver.com>
2022-04-03 19:37:31 +0900

    15651  N과 M (3)

--- Algorithm/B15651.java
+++ Algorithm/B15651.java
@@ -1,35 +1,71 @@
 package Algorithm;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class B15651 {
 
-    static int[] num = new int[10];
+    // 방법 1
+    // static int[] num = new int[10];
+    // public static StringBuilder go(int index, int n, int m){
+    //     if(m<=index){
+    //         StringBuilder sb = new StringBuilder();
+    //         for(int i=0;i<m;i++){
+    //             sb.append(num[i]);
+    //             if(i != m-1){
+    //                 sb.append(" ");
+    //             }
+    //         }
+    //         sb.append("\n");
+    //         return sb;
+    //     }
 
-    public static StringBuilder go(int index, int n, int m){
-        if(m<=index){
-            StringBuilder sb = new StringBuilder();
+    //     StringBuilder ans = new StringBuilder();
+    //     for(int i=0;i<n;i++){
+    //         num[index]=i+1;
+    //         ans.append(go(index+1, n,m));
+    //     }
+    //     return ans;
+    // }
+    // public static void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+    //     int m = sc.nextInt();
+    //     System.out.println(go(0,n,m));
+    // }
+
+    // 방법 2
+    static int[] arr = new int[10];
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void go(int index, int n, int m) throws IOException{
+        if(index == m){
             for(int i=0;i<m;i++){
-                sb.append(num[i]);
-                if(i != m-1){
-                    sb.append(" ");
-                }
+                //System.out.print(arr[i]+" ");
+                bw.write(arr[i]+" ");
             }
-            sb.append("\n");
-            return sb;
+            //System.out.println();
+            bw.write("\n");
+            return;
         }
-
-        StringBuilder ans = new StringBuilder();
-        for(int i=0;i<n;i++){
-            num[index]=i+1;
-            ans.append(go(index+1, n,m));
+        if(index>=m){
+            return;
         }
-        return ans;
+        for(int i=1;i<=n;i++){
+            arr[index]=i;
+            go(index+1, n,m);
+            arr[index]=0;
+        }
+        return;
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) throws IOException{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        System.out.println(go(0,n,m));
+        go(0,n,m);
+        bw.flush();
     }
 }
