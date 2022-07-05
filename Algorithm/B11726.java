8d30c74636f86e15b26aa43e5891707459b000a1
ahyoon99 <kimay9623@naver.com>
2022-07-06 00:37:48 +0900

    11726 2XN 타일링(Bottom-up)

--- Algorithm/B11726.java
+++ Algorithm/B11726.java
@@ -4,15 +4,15 @@ import java.util.*;
 
 public class B11726 {
     
-    static int go(int n, int[] answer){
-        if(answer[n]>0){
-            return answer[n];
-        }
-        if(answer[n]<=0){
-            answer[n]= (go(n-1,answer) + go(n-2,answer)) % 10007; 
-        }
-        return answer[n];
-    }
+    // static int go(int n, int[] answer){
+    //     if(answer[n]>0){
+    //         return answer[n];
+    //     }
+    //     if(answer[n]<=0){
+    //         answer[n]= (go(n-1,answer) + go(n-2,answer)) % 10007; 
+    //     }
+    //     return answer[n];
+    // }
     static public void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -21,8 +21,13 @@ public class B11726 {
         answer[2]=2;
 
         // 1. Top-down (재귀)
-        System.out.println(go(n, answer));
+        // System.out.println(go(n, answer));
 
+        // 2. Bottom-up (for문)
+        for(int i=3;i<=n;i++){
+            answer[i] = (answer[i-1]+answer[i-2]) % 10007;
+        }
+        System.out.println(answer[n]);
     }
     
 }
