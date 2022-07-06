da29e6e5c9969a6ed829aae96272d0a1cbed7d88
ahyoon99 <kimay9623@naver.com>
2022-07-06 18:28:11 +0900

    9095 1,2,3 더하기 (Bottom-up)

--- Algorithm/B9095.java
+++ Algorithm/B9095.java
@@ -30,28 +30,45 @@ public class B9095 {
     // }
 
     // 2. Top-down (재귀)
-    static int go(int n, int[] num){
-        if(n==1){
-            return 1;
-        }
-        if(n==2){
-            return 2;
-        }
-        if(n==3){
-            return 4;
-        }
-        else{
-            num[n] = go(n-1, num)+go(n-2, num)+go(n-3, num);
-        }
-        return num[n];
-    }
-    public static void main(String args[]){
+    // static int go(int n, int[] num){
+    //     if(n==1){
+    //         return 1;
+    //     }
+    //     if(n==2){
+    //         return 2;
+    //     }
+    //     if(n==3){
+    //         return 4;
+    //     }
+    //     else{
+    //         num[n] = go(n-1, num)+go(n-2, num)+go(n-3, num);
+    //     }
+    //     return num[n];
+    // }
+    // public static void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+    //     for(int i=0;i<n;i++){
+    //         int test = sc.nextInt();
+    //         int[] num = new int[test+1];
+    //         System.out.println(go(test,num));
+    //     }
+    // }
+
+    // 3. Bottom-up (for문)
+    static public void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=0;i<n;i++){
             int test = sc.nextInt();
-            int[] num = new int[test+1];
-            System.out.println(go(test,num));
+            int[] num = new int[11];
+            num[1]=1;
+            num[2]=2;
+            num[3]=4;
+            for(int j=4;j<=test;j++){
+                num[j]=num[j-1]+num[j-2]+num[j-3];
+            }
+            System.out.println(num[test]);
         }
     }
 }
