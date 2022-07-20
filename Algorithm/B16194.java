dab3a25b1110d869b7e5ebbb10fad5d0cae1a55d
ahyoon99 <kimay9623@naver.com>
2022-07-20 18:44:52 +0900

    16194 카드 구매하기2 (Top-down)

--- Algorithm/B16194.java
+++ Algorithm/B16194.java
@@ -3,6 +3,41 @@ import java.util.*;
 
 public class B16194 {
     // 1. Bottom-up
+    // static public void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+    //     int[] price = new int[n+1];
+    //     for(int i=1;i<=n;i++){
+    //         price[i]=sc.nextInt();
+    //     }
+    //     int[] min_price = new int[n+1];
+    //     min_price[1]=price[1];
+    //     for(int i=2;i<=n;i++){
+    //         min_price[i]=price[i];
+    //         for(int j=1;j<i;j++){
+    //             if(min_price[i]>price[j]+min_price[i-j]){
+    //                 min_price[i] = price[j]+min_price[i-j];
+    //             }
+    //         }
+    //     }
+    //     System.out.println(min_price[n]);
+    // } 
+    
+    // 2. Top-down
+    static int go(int n, int[] min_price, int[] price){
+        if(min_price[n]>0){
+            return min_price[n];
+        }
+        else{
+            min_price[n]=price[n];
+            for(int i=1;i<n;i++){
+                if(min_price[n]>price[i]+go(n-i,min_price, price)){
+                    min_price[n] = price[i]+go(n-i,min_price, price);
+                }
+            }
+        }
+        return min_price[n];
+    }
     static public void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -11,15 +46,6 @@ public class B16194 {
             price[i]=sc.nextInt();
         }
         int[] min_price = new int[n+1];
-        min_price[1]=price[1];
-        for(int i=2;i<=n;i++){
-            min_price[i]=price[i];
-            for(int j=1;j<i;j++){
-                if(min_price[i]>price[j]+min_price[i-j]){
-                    min_price[i] = price[j]+min_price[i-j];
-                }
-            }
-        }
-        System.out.println(min_price[n]);
-    }   
+        System.out.println(go(n,min_price, price));
+    }
 }
