e0a3eb5e2034fb435f2679223c4c5694026cccbc
ahyoon99 <kimay9623@naver.com>
2022-10-14 00:05:20 +0900

    2606 바이러스 (BFS - Queue)

--- Algorithm/B2606.java
+++ Algorithm/B2606.java
@@ -30,29 +30,61 @@ public class B2606 {
     // }
 
     // 방법 2 : DFS (Stack)
+    // static public void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     int n = sc.nextInt();
+    //     int k = sc.nextInt();
+    //     int[][] computer = new int[n+1][n+1];
+    //     boolean[] visited = new boolean[n+1];
+    //     for(int i=0;i<k;i++){
+    //         int a = sc.nextInt();
+    //         int b = sc.nextInt();
+    //         computer[a][b]=1;
+    //         computer[b][a]=1;
+    //     }
+    //     Stack<Integer> stack = new Stack<>();
+    //     int cnt=0;
+    //     stack.push(1);
+    //     visited[1]=true;
+    //     while(!stack.isEmpty()){
+    //         cnt++;
+    //         int num = stack.pop();
+    //         for(int i=0;i<=n;i++){
+    //             if(computer[num][i]==1 && visited[i]==false){
+    //                 visited[i]=true;
+    //                 stack.push(i);
+    //             }
+    //         }
+    //     }
+    //     System.out.println(cnt-1);
+    // }
+
+    // 방법 3 : BFS (Queue)
     static public void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int k = sc.nextInt();
-        int[][] computer = new int[n+1][n+1];
+        int m = sc.nextInt();
+        boolean[][] computer = new boolean[n+1][n+1];
         boolean[] visited = new boolean[n+1];
-        for(int i=0;i<k;i++){
+        for(int i=0;i<m;i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
-            computer[a][b]=1;
-            computer[b][a]=1;
+            computer[a][b]=true;
+            computer[b][a]=true;
         }
-        Stack<Integer> stack = new Stack<>();
-        int cnt=0;
-        stack.push(1);
+
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.offer(1);
         visited[1]=true;
-        while(!stack.isEmpty()){
+
+        int cnt=0;
+        while(!q.isEmpty()){
             cnt++;
-            int num = stack.pop();
+            int num = q.poll();
             for(int i=0;i<=n;i++){
-                if(computer[num][i]==1 && visited[i]==false){
+                if(computer[num][i]==true && visited[i]==false){
+                    q.add(i);
                     visited[i]=true;
-                    stack.push(i);
                 }
             }
         }
