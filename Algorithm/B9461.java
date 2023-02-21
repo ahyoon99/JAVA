57beb7e940de14e34ec852f9af0f9fd85f2daadd
ahyoon99 <kimay9623@naver.com>
2023-02-21 17:58:59 +0900

    9461 파도반 수열 (Top down)

--- Algorithm/B9461.java
+++ Algorithm/B9461.java
@@ -1,30 +1,63 @@
 package Algorithm;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class B9461 {
+    // 방법 1 : Bottom up
+    // static public void main(String args[]){
+    //     Scanner sc= new Scanner(System.in);
+    //     int t = sc.nextInt();
+    //     long[] p = new long[101];
+
+    //     p[0]=1;
+    //     p[1]=1;
+    //     p[2]=1;
+    //     p[3]=2;
+    //     p[4]=2;
+    //     p[5]=3;
+    //     p[6]=4;
+    //     p[7]=5;
+
+    //     for(int i=8;i<=100;i++){
+    //         p[i]=p[i-5]+p[i-1];
+    //     }
+
+    //     for(int i=0;i<t;i++){
+    //         int n = sc.nextInt();
+    //         System.out.println(p[n-1]);
+    //     }
+    // }
+    
+
+    // 방법 2 : Top down
     static public void main(String args[]){
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        long[] p = new long[101];
 
-        p[0]=1;
-        p[1]=1;
-        p[2]=1;
-        p[3]=2;
-        p[4]=2;
-        p[5]=3;
-        p[6]=4;
-        p[7]=5;
+        long[] result = new long[101];
+        Arrays.fill(result, -1);
+        result[0]=1;
+        result[1]=1;
+        result[2]=1;
+        result[3]=2;
+        result[4]=2;
+        result[5]=3;
+        result[6]=4;
+        result[7]=5;
 
-        for(int i=8;i<=100;i++){
-            p[i]=p[i-5]+p[i-1];
-        }
+        go(result, 100);
 
         for(int i=0;i<t;i++){
             int n = sc.nextInt();
-            System.out.println(p[n-1]);
+            System.out.println(result[n-1]);
         }
     }
-    
+
+    static long go(long[] result, int n){
+        if(result[n]!=-1){
+            return result[n];
+        }
+        result[n] = go(result, n-5) + go(result, n-1);
+        return result[n];
+    }   
 }
