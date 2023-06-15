01903f4b37c00d6fced2be1319608ab46dfb65d2
ahyoon99 <kimay9623@naver.com>
2023-06-15 21:32:17 +0900

    P43162 네트워크 (BFS)

---
+++ Algorithm/P43162.java
@@ -0,0 +1,46 @@
+package Algorithm;
+
+import java.util.*;
+
+public class P43162 {
+    static public void main(String args[]){
+        int n=3;
+        int[][] computers = {{1,1,0},{1,1,0},{0,0,1}};
+        
+        int result = solution(n, computers);
+        System.out.println(result);
+    }
+    
+    static public int solution(int n, int[][] computers) {
+        int answer = 0;
+        
+        boolean[] visited = new boolean[n];
+        
+        for(int i=0;i<n;i++){
+            if(visited[i]==false){
+                answer++;
+                BFS(i, n, computers, visited);
+            }
+        }
+        return answer;
+    }
+    
+    static public void BFS(int x, int n, int[][] computers, boolean[] visited){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(x);
+        visited[x]=true;
+        
+        while(!q.isEmpty()){
+            int num = q.poll();
+            for(int i=0;i<n;i++){
+                if(i==num){
+                    continue;
+                }
+                if(computers[num][i]==1 && visited[i]==false){
+                    q.add(i);
+                    visited[i]=true;
+                }
+            }
+        }
+    }
+}
