18204e081a3f4e4b3e6814c3082e89203d54be63
ahyoon99 <kimay9623@naver.com>
2024-04-15 14:35:15 +0900

    B1987 알파벳

--- Algorithm/B1987.java
+++ Algorithm/B1987.java
@@ -3,43 +3,63 @@ package Algorithm;
 import java.util.*;
 
 public class B1987 {
-    // 방법 1 : DFS(재귀)
+    static int n;
+    static int m;
     static int[] dx = {-1,0,1,0};
     static int[] dy = {0,1,0,-1};
-    static int ans = 0;
+    static char[][] matrix;
+    static boolean[] alp;
+    static int answer;
+    static int max;
 
-    static void dfs(char[][] map, boolean[] alp, int x, int y, int n, int m, int cnt){
-        char ch = map[x][y];
-        if(alp[ch-'A']==true){
-            if(ans<cnt){
-                ans = cnt;
-            }
-            return;
+    static class Node{
+        int x;
+        int y;
+
+        Node(){}
+        Node(int x, int y){
+            this.x=x;
+            this.y=y;
         }
-        alp[ch-'A'] = true;
-        for(int i=0;i<4;i++){
-            int nx = x+ dx[i];
-            int ny = y+ dy[i];
-            if(0<=nx && nx<n && 0<=ny && ny<m){
-                dfs(map,alp, nx, ny, n,m, cnt+1);
-            }
-        }
-        alp[ch-'A']=false;
-        return;
     }
+   
     static public void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        char[][] map = new char[n][m];
-        boolean[] alp = new boolean[26];
+        n = sc.nextInt();
+        m = sc.nextInt();
+        matrix = new char[n][m];
+        alp = new boolean[26];
+        answer=0;
+        max=0;
+        
         for(int i=0;i<n;i++){
             String com = sc.next();
             for(int j=0;j<m;j++){
-                map[i][j] = com.charAt(j);
+                matrix[i][j] = com.charAt(j);
             }
         }
-        dfs(map,alp,0,0,n,m,0);
-        System.out.println(ans);
+
+        alp[matrix[0][0]-'A']=true;
+        answer++;
+
+        backTracking(0,0);
+        System.out.println(max);
+    }
+
+    static void backTracking(int x, int y){
+        for(int i=0;i<4;i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(0<=nx && nx<n && 0<=ny && ny<m){
+                if(!alp[matrix[nx][ny]-'A']){
+                    alp[matrix[nx][ny]-'A']=true;
+                    answer++;
+                    backTracking(nx, ny);
+                    alp[matrix[nx][ny]-'A']=false;
+                    answer--;
+                }
+            }
+        }
+        max = Math.max(max, answer);
     }
 }
