39c8b91104dc6027e22c09a3dda48242d7888888
ahyoon99 <kimay9623@naver.com>
2024-11-14 16:07:55 +0900

    swea5215 햄버거 다이어트 (DP)

--- Algorithm/swea5215.java
+++ Algorithm/swea5215.java
@@ -24,7 +24,11 @@ public class swea5215 {
 		for(int testcase=1;testcase<=t;testcase++) {
 			System.out.print("#"+testcase+" ");
 			input(br, st);
-			solution();
+			// 방법 1
+			//solution();
+
+			// 방법 2
+			solution2();
 		}
 	}
 	
@@ -46,6 +50,27 @@ public class swea5215 {
 		go(length, idx+1, flaSum, calSum);
 	}
 	
+	static void solution2(){
+        int[][] dp = new int[n+1][l+1];
+
+        for(int i=0;i<=l;i++){
+            dp[0][i]=0;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=l;j++){
+                if(j-cal[i-1]>=0){
+                    dp[i][j] = Math.max(dp[i-1][j-cal[i-1]]+flavor[i-1], dp[i-1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        System.out.println(dp[n][l]);
+    }
+
 	static void input(BufferedReader br, StringTokenizer st) throws IOException{
 		st = new StringTokenizer(br.readLine(), " ");
 		n = Integer.parseInt(st.nextToken());
