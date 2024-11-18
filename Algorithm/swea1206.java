e8066f0a1e42f1242f77e6977b81584c6652be29
ahyoon99 <kimay9623@naver.com>
2024-11-18 10:16:08 +0900

    swea1206 View (방법 2)

--- Algorithm/swea1206.java
+++ Algorithm/swea1206.java
@@ -5,18 +5,61 @@ import java.io.*;
 
 public class swea1206 {
     static int n;
-    static int[] buildings;
     static int answer;
+
+    // 방법 1
+    static int[] buildings;
+
+    // 방법 2
+    static boolean[][] apt;
     static public void main(String args[]) throws IOException{
 
-        System.setIn(new FileInputStream("res/input_swea1206.txt"));
+        System.setIn(new FileInputStream("res/input.txt"));
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         for(int i=0;i<10;i++){
-            solution(br);
+            // 방법 1
+            // solution(br);
+            // System.out.println("#"+(i+1)+" "+answer);
+
+            // 방법 2
+            solution2(br);
             System.out.println("#"+(i+1)+" "+answer);
         }
+    }
+
+    static void solution2(BufferedReader br) throws IOException{
+        input2(br);
+        for(int i=2;i<n-2;i++){
+            for(int j=0;j<256;j++){
+                if(!apt[i][j]){
+                    break;
+                }
+                if(apt[i][j]){
+                    if(!apt[i-2][j] && !apt[i-1][j] && !apt[i+1][j] && !apt[i+2][j]){
+                        answer++;
+                    }
+                }
+            }
+        }
+    }
+
+    static void input2(BufferedReader br) throws IOException{
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        apt = new boolean[n][256];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0;i<n;i++){
+            int height = Integer.parseInt(st.nextToken());
+            for(int j=0;j<height;j++){
+                apt[i][j]=true;
+            }
+        }
+
+        answer = 0;
     }
     
     static public void solution(BufferedReader br) throws IOException{
